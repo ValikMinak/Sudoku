4747ebb0d3e9f9ad10986e4d3fdf83521e523c90
Valentyn Minakov <valikminak@gmail.com>
2020-08-31 14:56:52 +0300

    redux added

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,14 +1,35 @@
-import React, { FC } from 'react'
+import React, {FC} from 'react'
+import {useSelector, useDispatch} from "react-redux";
+import {Dispatch, AnyAction} from "redux";
 
-import { Container } from './styles'
+import {IReducer, selectBlock} from "reducers";
+
+import {Container} from './styles'
+import {INDEX, N} from "typings";
 
 interface IProps {
-  colIndex: number
-  rowIndex: number
+    colIndex: INDEX
+    rowIndex: INDEX
 }
 
-const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
-  return <Container data-cy={`block-${rowIndex}-${colIndex}`}>0</Container>
+interface IState {
+    value: N
+    isActive: boolean
+}
+
+const Block: FC<IProps> = ({colIndex, rowIndex}) => {
+    const state = useSelector<IReducer, IState>(({grid, selectedBlock}) => ({
+        isActive: selectedBlock ? selectedBlock[0] === rowIndex && selectedBlock[1] === colIndex : false,
+        value: grid ? grid[rowIndex][colIndex] : 0
+    }))
+    const dispatch = useDispatch<Dispatch<AnyAction>>()
+
+    function handleClick() {
+       if(!state.isActive) dispatch(selectBlock([rowIndex, colIndex]))
+    }
+
+    return <Container active={state.isActive} data-cy={`block-${rowIndex}-${colIndex}`}
+                      onClick={handleClick}>{state.value === 0 ? '' : state.value}</Container>
 }
 
 export default Block
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ThemeProvider } from 'styled-components'
+import {ThemeProvider} from 'styled-components'
+import {Provider} from "react-redux";
 
-import { Card, Content, Grid, Title } from 'components'
-import { unregister } from 'core'
-import { GlobalStyles, theme } from 'styles'
+import {Card, Content, Grid, Title} from 'components'
+import {unregister} from 'core'
+import {GlobalStyles, theme} from 'styles'
+import configureStore from "./core/configure-store";
 
+const store = configureStore();
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <Content data-cy="content">
-      <Title data-cy="title">Sudoku</Title>
-      <Card data-cy="card">
-        <Grid />
-      </Card>
-    </Content>
-  </ThemeProvider>,
-  document.getElementById('root')
+    <ThemeProvider theme={theme}>
+        <GlobalStyles/>
+        <Provider store={store}>
+            <Content data-cy="content">
+                <Title data-cy="title">Sudoku</Title>
+                <Card data-cy="card">
+                    <Grid/>
+                </Card>
+            </Content>
+        </Provider>
+    </ThemeProvider>,
+    document.getElementById('root')
 )
 
 unregister()
