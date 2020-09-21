36340ecc04f1e832ab46817d47f83ad770704439
ValikMinak <valikminak@gmail.com>
2020-09-21 22:15:36 +0300

    Updates

---
+++ precache-manifest.39d616468cf8669871ff7c15089ff7ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cbd874e15f622010a3117c95b81417e",
+    "url": "/Sudoku/index.html"
+  },
+  {
+    "revision": "6b7abde1d0dea9fe2f75",
+    "url": "/Sudoku/static/js/2.201ab5d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Sudoku/static/js/2.201ab5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13f53560f5717b5a91e",
+    "url": "/Sudoku/static/js/main.7cfd8968.chunk.js"
+  },
+  {
+    "revision": "635214cb19635f15a8b7",
+    "url": "/Sudoku/static/js/runtime-main.399dc15c.js"
+  }
+]);
